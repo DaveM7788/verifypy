bc62e63b1659a8a01057a9cc38985257d4fdf014
blue <david.mcdermott98@gmail.com>
2021-06-14 18:53:46 -0400

    Regenerate expected_responses
    Allows you regenerate expected responses by entering r/R. Also allows window to stay open so you can see results of running verifypy.py

--- verifypy.py
+++ verifypy.py
@@ -1,5 +1,15 @@
 import urlutils as ut
 
+def veryify_and_wait():
+    print("Verifying ...")
+    utils.verify()
+    rkey = input("Press r to regenerate expected_responses or any other key to quit ")
+    return rkey
+
 if __name__ == '__main__':
     utils = ut.UrlUtils()
-    utils.verify()
+    rkey = veryify_and_wait()
+    if rkey == 'r' or rkey == 'R':
+        print("Regenerating ...")
+        utils.generate()
+        veryify_and_wait()
